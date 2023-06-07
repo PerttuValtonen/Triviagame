ec4c35d98a08f1f3b27f686ff9e04890942ac196
PerttuValtonen <135586722+PerttuValtonen@users.noreply.github.com>
2023-06-07 12:43:31 +0300

    Initial Commit

---
+++ Triviagame/TriviagameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviagameApp.swift
+//  Triviagame
+//
+//  Created by Perttu Valtonen on 7.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviagameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
